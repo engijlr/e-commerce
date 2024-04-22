c6f5e7fb666d822225d54d7d73ca8a1cab7dc38d
Engi Lozada <engilozada@gmail.com>
2024-04-22 21:54:55 +0200

    Changing Header content and styles

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -1,7 +1,33 @@
-import React from 'react'
+'use client'
 
-function HeaderComponent({ header }) {
-  return <div>Header Component</div>
+import Link from 'next/link'
+import { Header } from '../../../../payload/payload-types'
+import { Gutter } from '../../Gutter'
+import Image from 'next/image'
+
+import classes from './index.module.scss'
+import { HeaderNav } from '../Nav'
+import { noHeaderFooterUrls } from '../../../constans'
+import { usePathname } from 'next/navigation'
+
+function HeaderComponent({ header }: { header: Header }) {
+  const pathname = usePathname()
+
+  return (
+    <nav
+      className={[classes.header, noHeaderFooterUrls.includes(pathname) && classes.hide]
+        .filter(Boolean)
+        .join(' ')}
+    >
+      <Gutter className={classes.wrap}>
+        <Link href="/">
+          <Image src="/logo-black.svg" alt="logo" width={170} height={50} />
+        </Link>
+
+        <HeaderNav header={header} />
+      </Gutter>
+    </nav>
+  )
 }
 
 export default HeaderComponent
